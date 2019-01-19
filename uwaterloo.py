13added0dcf2c6892567a1f425dd83523a143def
Atul Bipin <atulbipin@gmail.com>
2019-01-18 22:41:37 -0800

    Add all scripts

---
+++ uwaterloo.py
@@ -0,0 +1,31 @@
+import requests
+
+API_BASE_URL = "https://api.uwaterloo.ca/v2"
+
+class UWaterloo:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_courses(self):
+        payload = {
+            'key': self.api_key
+        }
+        
+        resp = requests.get(API_BASE_URL + "/courses.json", params = payload)
+
+        if (resp.status_code != 200):
+            print("ERROR CODE: {}".format(resp.status_code))
+            return []
+
+        courses = []
+
+        for course_data in resp.json()['data']:
+            courses.append({
+                'subject': course_data['subject'],
+                'catalog_number': course_data['catalog_number']
+            })
+
+        print(courses)
+
+uwaterloo = UWaterloo("55c795b34312e672729052f996ccda64")
+uwaterloo.get_courses()
