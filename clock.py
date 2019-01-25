1e53c83163405134707334b59da7a0c36a8e07b6
zahin <zahin.dev@gmail.com>
2019-01-25 04:29:48 +0000

    switched to cron

--- clock.py
+++ clock.py
@@ -1,25 +1,26 @@
-from pytz import utc
+from pytz import utc, timezone
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.combining import AndTrigger
 from apscheduler.triggers.interval import IntervalTrigger
 from apscheduler.triggers.cron import CronTrigger
+tz = timezone('EST')
+
 
 
 sched = BlockingScheduler()
 sched.configure(timezone=utc)
-
-pre_req_trigger = IntervalTrigger(weeks=3.5)
-post_req_trigger =IntervalTrigger(weeks=4)
+pre_req_trigger=CronTrigger(month='*', hour='0')
+post_req_trigger=CronTrigger(month='*', hour='12')
 
 
 def pre_req():
     os.system('python3 pre_req.py')
-    print('Running pre_req.py.')
+    print('Running pre_req.py at {}'.fromat(datetime.now(tz)))
 
 def post_req():
     os.system('python3 post_req.py')
-    print('Running post_req.py.')
+    print('Running post_req.py at {}'.format(datetime.now(tz)))
 
 sched.add_job(pre_req, pre_req_trigger)
 sched.add_job(post_req, post_req_trigger)
